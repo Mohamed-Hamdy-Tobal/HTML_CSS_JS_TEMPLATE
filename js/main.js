1e669e68aaa0328ba7dc54de9e46fe6f5205a2c3
Mohamed Tobal <mohamedtobal369@gmail.com>
2023-08-14 21:15:51 +0300

    UP

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+// -- For Settings --
+let setting = document.querySelector(".settings-box")
+let gear = document.querySelector(".settings-box .toggle-setting i")
+
+gear.onclick = function() {
+    // Toggle The Setting Icon
+    this.classList.toggle("fa-spin")
+    // Toggle The Setting Box
+    setting.classList.toggle("open")
+}
+
+// - For Select Color -
+let colorLi = document.querySelectorAll(".settings-box .option-box .colors-list li")
+
+// Loop On List Items
+colorLi.forEach(li => {
+
+    // For Click In Each Item
+    li.onclick = function() {
+
+        // Set The Color On Root
+        document.documentElement.style.setProperty("--main-color", li.dataset.color)
+    }
+})
+
+// -------------------------------------------------------------------------
+
+
 // Select Landing Page Element
 let landingPage = document.querySelector(".landing-page")
 
@@ -12,3 +40,4 @@ setInterval(() => {
 
     landingPage.style.backgroundImage = `url("images/${imgsArray[randomNumber]}")`
 }, 5000)
+
